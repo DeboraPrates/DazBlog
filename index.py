a35e05952c67a31d0dff6c5108494e5cc1db5981
DeboraPrates <173943736+DeboraPrates@users.noreply.github.com>
2024-09-30 16:52:16 -0300

    DazBlog1.1

--- index.py
+++ index.py
@@ -1,27 +1,61 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL  
+
+SITE = {
+    "name": "dazblog",
+    "owner": "Daz",
+    "logo": "",
+    "favicon": ""
+}
 
 app = Flask(__name__)
 
- # Rota para a página inicial
-@app.route('/')
+app.config["MYSQL_HOST"] = ""
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "dazblogdb"
+
+@app.route('/')  # Rota para a página inicial
 def home():
-    page = {
-        'title': 'Página Inicial',
-        'css': '/static/css/home.css',
-        'js': '/static/js/home.js'
+    toPage = {
+         "site": SITE,
+         "title": "",
+         "css":"home.css",
+         "js": "home.js"
     }
-    users = ('Maria', 'Joca', 'José', 'André')
-    return render_template ('home.html', page=page, listaUsers = users)
 
-@app.route('/contacts')
+app.route('/view/artid') # Rota para a página que exibe o artigo completo
+def view(artid):
+
+    sql = '''
+
+    '''
+    toPage = {
+         'site': SITE,
+         'title': '',
+         'css': 'view.css'
+    }
+    return render_template("view.html", page = toPage)
+
+@app.route('/contacts') # Rota para a página de contatos
 def contacts():
+        toPage = {
+             "site": SITE,
+             "title": "Faça Contato",
+             "css": "home.css"
+        }
 
-        page = {
-        'title': 'Página Inicial',
-        'css': 'home.css'
-    }
+        return render_template('contacts.html', page = toPage)
 
-    return render_template('contacts.html', page = page)
+@app.route('/about') # Rota para a página Sobre
+def about():
+     toPage = {
+          "site": SITE,
+          "title": "Sobre Nós",
+          "css": "about.css"
+     }
+
+     return render_template('about.html', page = toPage)
 
 
 if __name__ == '__main__':
